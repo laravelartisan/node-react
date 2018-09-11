5b6a6edb89bdc0a6edf5409d9bd2e382b2ac073b
nazim <nazimsoftpro@gmail.com>
2018-09-12 00:35:58 +0600

    initial setup done

---
+++ server/models/Article.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+let ArticleSchema = new mongoose.Schema({
+    text: String,
+    title: String,
+    description: String,
+    feature_img: String,
+    claps: Number,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [
+        {
+            author: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            text: String
+        }
+    ]
+})
+
+ArticleSchema.methods.clap = (c) => {
+    this.claps++
+    return this.save()
+}
+
+ArticleSchema.methods.comment = (c) => {
+    this.comments.push(c)
+    return this.save()
+}
+
+ArticleSchema.methods.addAuthor = (author_id) => {
+    this.author =  author_id
+    return this.save()
+}
+
+ArticleSchema.methods.getUserArticle = (_id) => {
+    Article.find({'author': _id}).then( article => article)
+}
+
+module.exports = mongoose.model('Article', ArticleSchema)
