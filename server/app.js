e7ba8f97e31a9e3718b05913a793b5edec45ff9f
nazim <nazimsoftpro@gmail.com>
2018-09-13 00:47:16 +0600

    react modules with redux added

--- server/app.js
+++ server/app.js
@@ -9,18 +9,20 @@ const cloudinary = require('cloudinary')
 const app = express()
 const router = express.Router()
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/medium"
+//const url = process.env.MONGODB_URI || "mongodb://sssamudra:ftfl10250700@ds255282.mlab.com:55282/medium"
 
 /** configure cloudinary */
 cloudinary.config({
-    cloud_name: 'YOUR_CLOUDINARY_NAME_HERE',
-    api_key: 'YOUR_CLOUDINARY_API_KEY_HERE',
-    api_secret: 'YOUR_CLOUDINARY_API_SECRET_HERE'
+    cloud_name: 'dfv6ygfvp',
+    api_key: '616142133365459',
+    api_secret: 'dGiEudWddILzxnDYYVJ9cAggpkE'
 })
 
 /** connect to MongoDB datastore */
 try {
     mongoose.connect(url, {
         //useMongoClient: true
+        useNewUrlParser: true
     })
 } catch (error) {
 
